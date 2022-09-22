be36fc1a3c21ed0ef3caf5fa8ba99f386b66d70c
ronategarcia <ronategarcia@csu.fullerton.edu>
2022-09-21 22:21:44 -0700

    all kinda done

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,27 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "header.h"
+
 using namespace std;
 
-int main()
-{
-  //create ifstream object and open readme file
-  ifstream inFile;
-  inFile.open("README.md");
-
-  //check for error when opening
-  if(inFile.fail()){
-    cout << "error opening file" << endl;
-    exit(1);
-  }
-
-  string name = " ";
-
-  while(!inFile.eof()){
-    inFile >> name;
-    cout << name;
-  }
-
-  inFile.close();
-    return 0;
+int main(){
+  string names = readandwrite();
+  cout << names;
 }
---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string readandwrite();
+
+#endif
