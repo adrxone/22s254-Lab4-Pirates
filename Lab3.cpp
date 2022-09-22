06b98f015487c7d0e29428486937b6d0cbfa193e
ronategarcia <73749301+ronategarcia@users.noreply.github.com>
2022-09-21 23:06:42 -0700

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -15,25 +15,3 @@ int main()
   cout << names;
   return 0;
 }
-=======
-  //create ifstream object and open readme file
-  ifstream inFile;
-  inFile.open("README.md");
-
-  //check for error when opening
-  if(inFile.fail()){
-    cout << "error opening file" << endl;
-    exit(1);
-  }
-
-  string name = " ";
-
-  while(!inFile.eof()){
-    inFile >> name;
-    cout << name;
-  }
-
-  inFile.close();
-    return 0;
-}
-
