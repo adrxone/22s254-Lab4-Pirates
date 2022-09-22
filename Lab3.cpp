9326c877d0157f667cf30daf0971a33adfc03ebc
Gilbert Galvan <gilbertgalvan@Gilberts-MacBook-Pro.local>
2022-09-21 20:00:08 -0700

    adding updated Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
+  //create ifstream object and open readme file
+  ifstream inFile;
+  inFile.open("README.md");
 
+  //check for error when opening
+  if(inFile.fail()){
+    cout << "error opening file" << endl;
+    exit(1);
+  }
+
+  string name = " ";
+
+  while(!inFile.eof()){
+    inFile >> name;
+    cout << name;
+  }
+
+  inFile.close();
     return 0;
 }
